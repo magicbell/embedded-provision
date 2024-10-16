0a24cba552601b45e25b10e96f4456277506fce1
Ullrich Schäfer <hi+github@ullrich.is>
2024-10-16 11:25:53 +0200

    Empty Project

---
+++ Tests/EmbeddedProvisionTests.swift
@@ -0,0 +1,9 @@
+import XCTest
+@testable import EmbeddedProvision
+
+class EmbeddedProvisionTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+}
